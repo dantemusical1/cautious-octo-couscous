9e881d57c5fdda21e32fd92469106776af725a7b
dantemusical1 <89634491+dantemusical1@users.noreply.github.com>
2022-12-09 17:46:37 -0400

    Create Lineasycomillas.cpp

---
+++ Lineasycomillas.cpp
@@ -0,0 +1,42 @@
+/*escribir una sentencia que nos muestre  lineas en consola
+
+	Hola soy "victor" y
+	estoy comenzando a programar
+	en c++ para volverme
+	un buen programador 
+*/
+
+
+
+
+
+
+
+
+/*
+
+para hacer que una comilla 
+pueda ser mostrada en la consola
+debemos precederla del simbolo \  
+oseaque debemos escribir:  \"
+
+y para dar un salto de linea 
+dentro de la consola ponemos 
+la letra n  precedida de un \
+como ocurre en el siguiente 
+comando:	 \n
+
+
+*/
+
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout<<"Hola soy \"victor\" y\nestoy comenzando a programar\nen c++ para volverme\nun buen programador "<<endl;	
+}
